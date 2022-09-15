2acbc8bdf3a8394cc37129097132f39cf273bab5
fateh bak <fatehbakiri6@gmail.com>
2022-09-15 17:32:09 +0200

    errors

--- routes/routes.js
+++ routes/routes.js
@@ -2,16 +2,38 @@ const express = require('express')
 const cors = require('cors')
 const router = express.Router()
 const db = require("../database/database");
+const database = require('../../../00-starting-project/data/database');
 
 
 router.get("/",async function (req,res){
     console.log("requested recieved")
-    database = await db.getDb()
+    const database = await db.getDb()
     articles = await database.collection('articles').find().toArray()
     articles = JSON.stringify(articles)
     res.status(200)
     res.json(articles)
 })
 
+router.post('/',function (req,res){
+    console.log("recieved")
+    console.log(JSON.parse(req.body))
+    // // creating user
+    // const user = {
+    //     name: req.body.name,
+    //     email:req.body.email,
+    //     password:req.body.password
+    // } 
+    // // getting users from database
+    // database = await db.getDb()
+    // userexist = await database.collection('users').find({email:user.name}).toArray
 
+    // if(!userexist){
+    //    await database.collection('users').insertOne({user})
+    //     res.json({message: "succes, user registred!", registred:true})
+    // }else{
+    //     res.json({message: "failed to register, this user alredy exist on our database", registred:false})
+    // }
+
+
+})
 module.exports={router:router}
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const { router } = require("./routes/routes");
 const db = require("./database/database");
 const port = 3001;
-const cors = require('cors')
+const cors = require('cors');
+app.use(express.urlencoded({extended:false}))
 app.use(cors())
 app.use(router);
-
-db.connectToDb().then(app.listen(port))
+db.connectToDb().then(()=>app.listen(port))
