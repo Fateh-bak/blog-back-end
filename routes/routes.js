704e17eea2ca64ac03c5e01005da6d7c380b9586
fateh bak <fatehbakiri6@gmail.com>
2022-09-16 02:46:54 +0200

    whene recieving JSON data we need to implement json parser

--- routes/routes.js
+++ routes/routes.js
@@ -14,26 +14,11 @@ router.get("/",async function (req,res){
     res.json(articles)
 })
 
-router.post('/',function (req,res){
-    console.log("recieved")
-    console.log(JSON.parse(req.body))
-    // // creating user
-    // const user = {
-    //     name: req.body.name,
-    //     email:req.body.email,
-    //     password:req.body.password
-    // } 
-    // // getting users from database
-    // database = await db.getDb()
-    // userexist = await database.collection('users').find({email:user.name}).toArray
-
-    // if(!userexist){
-    //    await database.collection('users').insertOne({user})
-    //     res.json({message: "succes, user registred!", registred:true})
-    // }else{
-    //     res.json({message: "failed to register, this user alredy exist on our database", registred:false})
-    // }
-
-
+router.post('/sendCredentials',async function(req,res){
+    console.log(req.body)
+    const body = {message:"data recieved"}
+    const data = JSON.stringify(body)
+    res.json(data)
 })
+
 module.exports={router:router}
--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const { router } = require("./routes/routes");
 const db = require("./database/database");
 const port = 3001;
 const cors = require('cors');
-app.use(express.urlencoded({extended:false}))
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.use(cors())
 app.use(router);
 db.connectToDb().then(()=>app.listen(port))
